0fbe46d8645bfe968df84eba53cfeca734079097
lpontes7 <lucas.pontes.07@hotmail.com>
2020-11-23 08:56:39 -0300

    primeiro commit

---
+++ app/src/main/java/com/lpontes7/desmotivacao/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lpontes7.desmotivacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
