c484774c113a1b0db07b8e8d253a500fc23953ce
lpontes7 <lucas.pontes.07@hotmail.com>
2020-11-23 09:28:22 -0300

    Adicionando elementos da primeira tela

---
+++ app/src/main/java/com/lpontes7/desmotivacao/Welcome.kt
@@ -0,0 +1,12 @@
+package com.lpontes7.desmotivacao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Welcome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
